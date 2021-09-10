f9cd955b58bb354267144ef6c4163627f6266a58
emiliano9266 <emiliano9266@gmail.com>
2021-09-10 18:05:58 -0300

    Add annotation to specify operations

--- src/main/java/com/wenance/challenger/resources/BTCResource.java
+++ src/main/java/com/wenance/challenger/resources/BTCResource.java
@@ -3,6 +3,7 @@ package com.wenance.challenger.resources;
 import com.wenance.challenger.services.BTCAverage;
 import com.wenance.challenger.repository.BTCPrice;
 import com.wenance.challenger.services.BTCService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,19 +25,22 @@ public class BTCResource {
     }
 
     @GetMapping
+    @Operation(summary = "Find all")
     public ResponseEntity<List<BTCPrice>> findAll() {
         return ResponseEntity.ok(this.btcService.findAll());
     }
 
     @GetMapping("/")
+    @Operation(summary = "Find by timestamp")
     public ResponseEntity<BTCPrice> findByDateTime(
-            @RequestParam(value = "dateTime", required = false)
+            @RequestParam(value = "dateTime")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime
     ) {
         return ResponseEntity.ok(this.btcService.findByDateTime(dateTime));
     }
 
     @GetMapping("/average")
+    @Operation(summary = "Get average")
     public ResponseEntity<BTCAverage> average(
             @RequestParam(value = "from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
             @RequestParam(value = "to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to
